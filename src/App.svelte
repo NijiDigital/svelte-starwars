369bb53a5088c3acc98c7c103cefd4dc96fa68bf
Olivier Penhoat <openhoat@gmail.com>
2022-10-12 07:50:05 +0200

    Step1

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<div class="amber-text">
+	<h1 class="center-align">Star Wars</h1>
+	<main class="section amber lighten-4 black-text">
+	</main>
+</div>
+
+<style>
+    main {
+        padding: 1em;
+        margin: 0 auto;
+    }
+
+    h1 {
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 600;
+    }
+</style>
